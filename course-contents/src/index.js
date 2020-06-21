08ebc2bb4c74ef7b00bb808cc280c5cdbd5f693f
Sean Yeo <seansqyeo@gmail.com>
2020-06-20 17:19:37 -0700

    finish 2.1, unused vars total and part for later maybe

---
+++ course-contents/src/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <ul>
+        {parts.map(part => 
+          <li key={part.id}>
+            {part.name}
+          </li>)}
+      </ul>
+    </div>
+  )
+}
+
+const Course = ({course}) => {//how to decompose object in prop?
+  //should I decompose object in prop? bad practice?
+  const {id, name, parts} = course
+  console.log("id", id);
+  console.log("name", name);
+  console.log("parts", parts);
+  
+  
+  
+  return (
+    <div>
+      <Header text={name} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
