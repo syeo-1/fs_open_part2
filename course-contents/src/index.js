14bf4a0e6d76d79ff5781682f0571424265c949c
Sean Yeo <seansqyeo@gmail.com>
2020-06-20 21:15:49 -0700

    finish 2.4

--- course-contents/src/index.js
+++ course-contents/src/index.js
@@ -51,30 +51,68 @@ const Course = ({course}) => {//how to decompose object in prop?
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-  return <Course course={course} />
+const Courses = ({courses}) => {
+  return (
+    <div>
+      {courses.map(course =>
+        <Course course={course} />
+      )}
+    </div>
+  )
 }
 
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+
+    
+  ]
+
+  return(
+    <div>
+      <Courses courses={courses} />
+    </div>
+  )
+}
 ReactDOM.render(<App />, document.getElementById('root'))
