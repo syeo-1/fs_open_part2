3ec6f2d6974ebfad6e6efcdbb44fc4df7b2aee16
Sean Yeo <seansqyeo@gmail.com>
2020-06-22 23:42:19 -0700

    finish 2.9

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -11,9 +11,16 @@ const Numbers = ({persons}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterNumbers, setFilterNumbers] = useState(true)
+  const [searchString, setSearchString] = useState('')
 
   const addData = (event) => {
     event.preventDefault()
@@ -49,10 +56,33 @@ const App = () => {
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value)
   }
+
   
+  const personsToShow = filterNumbers 
+    ? persons.filter(person => person.name.toLowerCase().includes(searchString))
+    : persons 
+  const handleSearchString = (event) => {
+    // console.log(event.target.value);
+    setSearchString(event.target.value)
+
+    searchString.length === 0 ? setFilterNumbers(false) : setFilterNumbers(true)
+    
+    
+    // console.log("persons to show", personsToShow);
+    
+    // setPersons(personsToShow)
+  }
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={searchString} onChange={handleSearchString}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addData}>
         <div>
           name: <input value={newName} onChange={handleNewName}/>
@@ -65,7 +95,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons}/>
+      <Numbers persons={personsToShow}/>
     </div>
   )
 }
