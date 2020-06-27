e8a85e07213adeee841e1addd1003c05f5bc0917
Sean Yeo <seansqyeo@gmail.com>
2020-06-26 18:09:11 -0700

    finish 2.11

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({searchString, handleSearchString}) => (
   <form>
@@ -33,16 +34,21 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterNumbers, setFilterNumbers] = useState(true)
   const [searchString, setSearchString] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addData = (event) => {
     event.preventDefault()
